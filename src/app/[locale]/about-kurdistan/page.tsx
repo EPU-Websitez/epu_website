6f0862a280ab4ab1471afa8308a8274d79a7bec7
Mohammed Omar <mohammedomar@zibox.io>
2025-06-29 19:31:00 +0300

    Doing about kurdistan and tutorial pages

--- src/app/[locale]/about-kurdistan/page.tsx
+++ src/app/[locale]/about-kurdistan/page.tsx
@@ -17,6 +17,65 @@ const page = () => {
             className="w-full h-full"
           />
         </div>
+        <h2 className="font-semibold text-titleNormal text-secondary">
+          {t("general_background")}
+        </h2>
+        <span className="text-secondary text-smallParagraph">
+          {t("general_text")}
+        </span>
+        <div className="flex_center gap-10 w-full md:flex-row flex-col mt-20">
+          <div className="w-1/2 h-[380px] relative md:block hidden">
+            <Image
+              src={"/images/kurdistan-nature.png"}
+              alt="My Image"
+              fill
+              priority
+              className="w-full h-full"
+            />
+          </div>
+          <div className="flex_start flex-col gap-5 text-secondary md:w-1/2 w-full">
+            <h3 className="relative font-semibold z-10 text-titleNormal md:ltr:-ml-6 ltr:ml-6 md:rtl:-mr-6 rtl:m-6">
+              <span className="z-10 relative">{t("nature")}</span>
+              <span className="md:w-full w-[110%] h-[14px] absolute md:ltr:right-[30%] ltr:right-0 md:rtl:left-[30%] rtl:right-0 bottom-2 bg-golden"></span>
+            </h3>
+            <div className="w-full sm:h-[380px] h-[300px] md:hidden block relative">
+              <Image
+                src={"/images/kurdistan-nature.png"}
+                alt="My Image"
+                fill
+                priority
+                className="w-full h-full"
+              />
+            </div>
+            <span className="text-sm font-medium">{t("general_text")}</span>
+          </div>
+        </div>
+        <div className="flex_center gap-10 w-full md:flex-row flex-col mt-10">
+          <div className="flex_start flex-col gap-5 text-secondary md:w-1/2 w-full">
+            <h3 className="relative font-semibold z-10 text-titleNormal">
+              {t("culture")}
+            </h3>
+            <div className="w-full sm:h-[380px] h-[300px] md:hidden block relative">
+              <Image
+                src={"/images/kurdistan-nature.png"}
+                alt="My Image"
+                fill
+                priority
+                className="w-full h-full"
+              />
+            </div>
+            <span className="text-sm font-medium">{t("general_text")}</span>
+          </div>
+          <div className="w-1/2 h-[380px] relative md:block hidden">
+            <Image
+              src={"/images/kurdistan-culture.png"}
+              alt="My Image"
+              fill
+              priority
+              className="w-full h-full"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/app/[locale]/tutorials/page.tsx
@@ -0,0 +1,143 @@
+import SubHeader from "@/components/subHeader";
+import { useTranslations } from "next-intl";
+import Image from "next/image";
+import Link from "next/link";
+import { CiSearch } from "react-icons/ci";
+import { FaPlay } from "react-icons/fa6";
+
+const page = () => {
+  const t = useTranslations("Tutorials");
+  return (
+    <div className="my-10 flex_center w-full">
+      <div className="max-w-[1024px] px-3 w-full flex_start flex-col gap-8">
+        <SubHeader title={t("head")} />
+        <div className="w-full flex_center sm:gap-10 gap-3">
+          <div className="relative w-full h-14">
+            <span className="pointer-events-none text-black opacity-50 absolute ltr:left-5 right-5 top-1/2 -translate-y-1/2 z-10 text-2xl">
+              <CiSearch />
+            </span>
+            <input
+              type="text"
+              className="h-full w-full bg-lightBorder px-14 rounded-2xl  focus:border-primary outline-none focus:outline-primary"
+              placeholder={t("search")}
+            />
+          </div>
+          <button className="sm:px-8 px-4 flex-shrink-0 h-14 rounded-2xl bg-gradient-to-r from-primary to-blue text-white">
+            {t("search_button")}
+          </button>
+        </div>
+        <div className="grid sm:grid-cols-2 grid-cols-1 w-full gap-10 mt-10">
+          <div className="w-full flex_start flex-col md:gap-4 gap-2 group relative">
+            <Link
+              href={"/"}
+              className="relative w-full md:h-[330px] h-[200px] rounded-xl overflow-hidden"
+            >
+              <div className=" w-32 h-7 flex_center rounded-full z-10 absolute top-2 ltr:left-2 rtl:right-2">
+                <Image
+                  src="/images/logo.svg"
+                  alt="title"
+                  fill
+                  priority
+                  className="w-full h-auto object-cover group-hover:scale-105 duration-300"
+                />
+              </div>
+              <Image
+                src="/images/park.png"
+                alt="title"
+                fill
+                priority
+                className="w-full h-auto object-cover group-hover:scale-105 duration-300"
+              />
+              <button className="flex_center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-12 h-12 text-white rounded-full text-xl bg-gradient-to-r from-[#DCC48C] to-[#FFA64D] z-10">
+                <FaPlay />
+              </button>
+              <a
+                href="#"
+                title="title"
+                className="absolute bottom-3 ltr:left-0 right-0 ltr:rounded-r-full rounded-l-full bg-black bg-opacity-35 px-3 py-2 text-white text-sm"
+              >
+                {t("watch")}
+              </a>
+            </Link>
+            <h3 className="md:text-lg text-base font-medium hover:text-primary text-secondary duration-300">
+              Introduction to Moodle
+            </h3>
+          </div>
+          <div className="w-full flex_start flex-col md:gap-4 gap-2 group relative">
+            <Link
+              href={"/"}
+              className="relative w-full md:h-[330px] h-[200px] rounded-xl overflow-hidden"
+            >
+              <div className=" w-32 h-7 flex_center rounded-full z-10 absolute top-2 ltr:left-2 rtl:right-2">
+                <Image
+                  src="/images/logo.svg"
+                  alt="title"
+                  fill
+                  priority
+                  className="w-full h-auto object-cover group-hover:scale-105 duration-300"
+                />
+              </div>
+              <Image
+                src="/images/park.png"
+                alt="title"
+                fill
+                priority
+                className="w-full h-auto object-cover group-hover:scale-105 duration-300"
+              />
+              <button className="flex_center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-12 h-12 text-white rounded-full text-xl bg-gradient-to-r from-[#DCC48C] to-[#FFA64D] z-10">
+                <FaPlay />
+              </button>
+              <a
+                href="#"
+                title="title"
+                className="absolute bottom-3 left-0 rounded-r-full bg-black bg-opacity-35 px-3 py-2 text-white text-sm"
+              >
+                Watch On Youtube
+              </a>
+            </Link>
+            <h3 className="md:text-lg text-base font-medium hover:text-primary text-secondary duration-300">
+              Introduction to Moodle
+            </h3>
+          </div>
+          <div className="w-full flex_start flex-col md:gap-4 gap-2 group relative">
+            <Link
+              href={"/"}
+              className="relative w-full md:h-[330px] h-[200px] rounded-xl overflow-hidden"
+            >
+              <div className=" w-32 h-7 flex_center rounded-full z-10 absolute top-2 ltr:left-2 rtl:right-2">
+                <Image
+                  src="/images/logo.svg"
+                  alt="title"
+                  fill
+                  priority
+                  className="w-full h-auto object-cover group-hover:scale-105 duration-300"
+                />
+              </div>
+              <Image
+                src="/images/park.png"
+                alt="title"
+                fill
+                priority
+                className="w-full h-auto object-cover group-hover:scale-105 duration-300"
+              />
+              <button className="flex_center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-12 h-12 text-white rounded-full text-xl bg-gradient-to-r from-[#DCC48C] to-[#FFA64D] z-10">
+                <FaPlay />
+              </button>
+              <a
+                href="#"
+                title="title"
+                className="absolute bottom-3 left-0 rounded-r-full bg-black bg-opacity-35 px-3 py-2 text-white text-sm"
+              >
+                Watch On Youtube
+              </a>
+            </Link>
+            <h3 className="md:text-lg text-base font-medium hover:text-primary text-secondary duration-300">
+              Introduction to Moodle
+            </h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default page;
